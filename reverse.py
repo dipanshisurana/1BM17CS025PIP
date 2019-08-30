6a2ddf76c25b90a8c1a9fc193609f74de6c315d4
dipanshisurana <54200072+dipanshisurana@users.noreply.github.com>
2019-08-30 10:04:17 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,6 +1,13 @@
 def reverse(str):
     str1=str[::-1]
-    print(str1)
+    li=[]
+    li=str.split(" ")
+    li.reverse()
+
+    for i in li:
+        print(i,end=" ")
+        
+    print("\n",str1)
 
 str=input("enter the string \n")
 reverse(str)
